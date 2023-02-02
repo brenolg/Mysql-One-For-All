8390c65dfe64f508152810fe77c7920c2cc0596d
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2023-02-02 13:34:00 -0300

    adcionando modelo ao repositorio

--- desafio7.sql
+++ desafio7.sql
@@ -10,3 +10,18 @@ ON AR.artista_id = S.artista_id
 
 GROUP BY artista, album
 ORDER BY seguidores DESC, artista, album;
+
+-- Seleciona albuns num array
+
+-- SELECT AR.artista AS artista,
+-- JSON_ARRAYAGG(AL.album) AS  album,
+-- COUNT(S.usuario_id) AS seguidores
+
+-- FROM SpotifyClone.seguindo_artistas AS S
+-- INNER JOIN  SpotifyClone.albuns AS AL
+-- ON S.artista_id = AL.artista_id
+-- INNER JOIN SpotifyClone.artistas AS AR
+-- ON AR.artista_id = S.artista_id
+
+-- GROUP BY artista
+-- ORDER BY seguidores DESC, artista, album;
